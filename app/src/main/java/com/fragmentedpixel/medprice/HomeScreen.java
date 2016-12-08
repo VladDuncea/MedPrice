ea61832977366175d813e34173e96755504806b6
VladDuncea <vlad_duncea_31@yahoo.com>
2016-12-08 22:11:53 +0200

    update

--- app/src/main/java/com/fragmentedpixel/medprice/HomeScreen.java
+++ app/src/main/java/com/fragmentedpixel/medprice/HomeScreen.java
@@ -2,14 +2,35 @@ package com.fragmentedpixel.medprice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
 
 public class HomeScreen extends AppCompatActivity {
-
+    private EditText search_bar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        link();
     }
+
+    private void link()
+    {
+        search_bar=(EditText) findViewById(R.id.editText);
+        search_bar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                refresh();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
 
 
 }
